95298e2efe2c84583477e9e7419dee967ca284cf
Mariia Synoverska <mariykasinover@gmail.com>
2018-12-22 10:46:40 +0200

    Updates

---
+++ precache-manifest.e50e1b1f04aad7f41670f3ba36df3351.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "688a3e402f3e03b3c308579967dd31db",
+    "url": "/BikesShop/static/media/logo.688a3e40.png"
+  },
+  {
+    "revision": "3c5673b468122c2c68234a45cc2e7f04",
+    "url": "/BikesShop/static/media/icon-cart.3c5673b4.png"
+  },
+  {
+    "revision": "3bddb32659642e4de9ac6b13837f919a",
+    "url": "/BikesShop/static/media/bg.3bddb326.jpg"
+  },
+  {
+    "revision": "f975f038661ef5cfd19b",
+    "url": "/BikesShop/static/js/runtime~main.f975f038.js"
+  },
+  {
+    "revision": "b66fe2ad40e76435ee7a",
+    "url": "/BikesShop/static/js/main.b66fe2ad.chunk.js"
+  },
+  {
+    "revision": "f302078ef3c2012f7933",
+    "url": "/BikesShop/static/js/1.f302078e.chunk.js"
+  },
+  {
+    "revision": "b66fe2ad40e76435ee7a",
+    "url": "/BikesShop/static/css/main.4516dc29.chunk.css"
+  },
+  {
+    "revision": "33f1540b16d17f602618479c1c5724a4",
+    "url": "/BikesShop/index.html"
+  }
+];
